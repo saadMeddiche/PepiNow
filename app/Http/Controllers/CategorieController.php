de853945ea1a6a6a8fea0de1f786b82eee9d1fde
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-03-14 11:36:30 +0100

    CRUD Of Categorie

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -4,82 +4,87 @@ namespace App\Http\Controllers;
 
 use App\Models\Categorie;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategorieFormValidation;
 
 class CategorieController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Show All Categories
     public function index()
     {
-        //
+        //fetch all Categories
+        $Categories = Categorie::all();
+
+        //return all Categories in json format
+        return response()->json(['Categories' => $Categories]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    //Add Categorie
+    public function store(CategorieFormValidation $request)
     {
-        //
+        //store the validated data from the request , and store it in an associative array
+        $data = $request->validated();
+
+        //new instance
+        $Categorie = new Categorie();
+
+        //Affect the validated data to the new object
+        $Categorie->nom = $data["nom"];
+        
+        //Insert the new Categorie to the DB
+        $Categorie->save();
+
+        //Return a success message
+        return response()->json(['message' => 'Categorie Added Successfuly']);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    //Show Categories
+    public function show($id)
     {
-        //
+        //Fetch Categorie
+        $Categorie = Categorie::find($id);
+
+        //Return a "Fail Message" if no categorie has been found with that given ID
+        if (!$Categorie) return response()->json(['message' => 'No Categorie Found'] , 404);
+            
+        //Return The Categorie in json format
+        return response()->json(['Categories' => $Categorie] , 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Categorie $categorie)
+    //Update Categorie
+    public function update(CategorieFormValidation $request, $id)
     {
-        //
+        //store the validated data from the request , and store it in an associative array
+        $data = $request->validated();
+
+        // Find the choosen Categorie to update
+        $Categorie = Categorie::find($id);
+
+        //Return a "Fail Message" if no Categorie has been found with that given ID
+        if (!$Categorie)  return response()->json(['message' => 'No Categorie Found'], 404);
+
+        //afect the new data to the choosen object
+        $Categorie->nom = $data["nom"];
+       
+        //Update the data of that Categorie
+        $Categorie->update();
+
+        //Return a success message
+        return response()->json(['message' => 'Categorie Updated Successfuly'], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Categorie $categorie)
+    //Delete Categorie
+    public function destroy($id)
     {
-        //
-    }
+        //Find the choosen Categorie to delete
+        $Categorie = Categorie::find($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Categorie $categorie)
-    {
-        //
-    }
+        //Return a "Fail Message" if no Categorie has been found with that given ID
+        if (!$Categorie)  return response()->json(['message' => 'Categorie Not Found'],404);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Categorie $categorie)
-    {
-        //
+        //delete choosed Categorie
+        $Categorie->delete();
+
+        //Return a success message
+        return response()->json(['message' => 'Categorie Deleted Successfuly'],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*================================Categories================================*/
+/* Show All Categories */
+Route::get('Categories', [CategorieController::class, 'index']);
+
+/* Show One Categorie */
+Route::get('Categorie/{id}', [CategorieController::class, 'show']);
+
+/* Add A Categorie */
+Route::post('Categorie', [CategorieController::class, 'store']);
+
+/* Update A Categorie */
+Route::put('Categorie/{id}', [CategorieController::class, 'update']);
+
+/* Delete A Categorie */
+Route::delete('Categorie/{id}', [CategorieController::class, 'destroy']);
+    /*================================End Categories================================*/
